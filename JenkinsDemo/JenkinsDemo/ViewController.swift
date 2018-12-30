520d6d4797e43289618f450b229b1b96996fb9d4
Mohd Haider <mdhaider1811@gmail.com>
2018-12-30 20:28:05 +0530

    Added a new iOS Project.

---
+++ JenkinsDemo/JenkinsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsDemo
+//
+//  Created by Mohd Haider on 30/12/18.
+//  Copyright © 2018 Mohd Haider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
